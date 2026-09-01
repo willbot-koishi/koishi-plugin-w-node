74770c86368f42df4b1ea69eb62addbc4b89dc18
173gvQY8MFaDk9Jt <g@v.q>
2026-09-02 03:35:08 +0800

    fix(w-node): Add read-write locks to avoid concurrent conflicts in packet operations.

--- src/utils.ts
+++ src/utils.ts
@@ -32,3 +32,62 @@ export namespace PackageInfo {
 }
 
 export const VERSION_SYMBOL = '@'
+
+export class ReadWriteLock {
+  private readMap: Record<symbol, true> = {}
+  private reading: Promise<void> = null
+  private readingRes: (value: (PromiseLike<void> | void)) => void = null
+  private writeQueue: ((value: (PromiseLike<void> | void)) => void)[] = []
+  private writing: Promise<void> = null
+  private writingRes: (value: (PromiseLike<void> | void)) => void = null
+
+  async r<T>(cb: () => T | Promise<T>): Promise<T> {
+    if (this.writing) {
+      await this.writing
+    }
+    if (! this.reading) {
+      this.reading = new Promise(resolve => this.readingRes = resolve)
+    }
+    const key = Symbol('read')
+    this.readMap[key] = true
+    try {
+      return await cb()
+    }
+    finally {
+      delete this.readMap[key]
+      if (Object.getOwnPropertySymbols(this.readMap).length === 0) {
+        this.readingRes()
+        this.reading = null
+        this.readingRes = null
+      }
+    }
+  }
+
+  async w<T>(cb: () => T | Promise<T>): Promise<T> {
+    if (this.reading) {
+      await this.reading
+    }
+    if (! this.writing) {
+      this.writing = new Promise(resolve => this.writingRes = resolve)
+    }
+    else {
+      let res: (value: unknown) => void
+      const lock = new Promise(resolve => res = resolve)
+      this.writeQueue.push(res)
+      await lock
+    }
+    try {
+      return await cb()
+    }
+    finally {
+      if (this.writeQueue.length === 0) {
+        this.writingRes()
+        this.writing = null
+        this.writingRes = null
+      }
+      else {
+        this.writeQueue.shift()()
+      }
+    }
+  }
+}
